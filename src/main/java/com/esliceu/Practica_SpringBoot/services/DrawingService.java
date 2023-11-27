6b7ace7640ae6b387891544a5478d5eba54bfb26
pau <pauramisnunez@gmail.com>
2023-11-27 18:25:58 +0100

    css & js accepted

--- src/main/java/com/esliceu/Practica_SpringBoot/services/DrawingService.java
+++ src/main/java/com/esliceu/Practica_SpringBoot/services/DrawingService.java
@@ -12,25 +12,7 @@ public class DrawingService {
     DrawingRepo drawingRepo;
 
     public void saveDrawing(Drawing drawing) {
-        int id = genRandId();
-        drawing.setId(id);
-        System.out.println("id = " + id);
         drawingRepo.storeDrawing(drawing);
-    }
-
-    private int genRandId() {
-        Random rand = new Random();
-        int id = rand.nextInt(10000);
-
-        System.out.println("id." + id);
-
-        //TODO second go breaks
-        /*if(idUsed(id)) {
-            id = genRandId();
-        }*/
-
-        System.out.println("id generated:" + id);
-        return id;
     }
 
     public List<Drawing> showDrawings() {
@@ -43,10 +25,6 @@ public class DrawingService {
 
     public Drawing getDrawingById(int id) {
         return drawingRepo.getDrawingById(id);
-    }
-
-    public boolean idUsed(int id){
-        return drawingRepo.idUsed(id);
     }
 
     public void deleteDrawing(int id){ drawingRepo.deleteDrawing(id); }
